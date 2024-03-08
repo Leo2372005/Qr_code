7b4887e65e3efc82a5a4064f370d7770d4f6691b
Leo2372005 <rsingam464@gmail.com>
2024-03-08 18:44:21 +0530

    Profile_card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import Qr from './Qr'
+import Pro from './Pro.jsx'
 const App = () => {
-  return (  
-    <Qr />
-  )
+  return ( 
+    <>
+    <Pro Img="https://source.unsplash.com/random/150x150?boy?1" Name="Rajasingam" Course="Full-Strack" Age={18} Blood="O+" Address="413/South Street,Kothankulam" No={8838411480}/>
+     <Pro Img="https://source.unsplash.com/random/150x150?boy?2" Name="Ravi" Course="Back-end" Age={18} Blood="O+" Address="413/South Street,Kothankulam" No={8838411480}/>
+    <Pro Img="https://source.unsplash.com/random/150x150?boy?3" Name="Mani" Course="Python" Age={18} Blood="O+" Address="413/South Street,Kothankulam" No={8838411480}/>  
+    </>
+    )
 }
 
 export default App
